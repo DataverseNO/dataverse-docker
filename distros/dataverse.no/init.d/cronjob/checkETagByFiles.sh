9548454ff055f6a0b32fba596522ed587abb4cb9
Benedikt Meier <benedikt.meier@uit.no>
2024-04-04 11:04:33 +0200

    check if file exist

--- distros/dataverse.no/init.d/cronjob/checkETagByFiles.sh
+++ distros/dataverse.no/init.d/cronjob/checkETagByFiles.sh
@@ -22,33 +22,35 @@ fi
 #while read p; do
 while true; do
 
-	line=$(head -n 1 /mnt/dataverse_checkETag.txt)
+	if [ -f "/mnt/dataverse_checkETag.txt" ]; then
+		line=$(head -n 1 /mnt/dataverse_checkETag.txt)
 
-	IFS=' ' read -a arrayData <<< "$line"
-	#echo ${arrayData[0]}
+		IFS=' ' read -a arrayData <<< "$line"
+		#echo ${arrayData[0]}
 
-	s3ETag=$(aws s3api --endpoint https://$aws_endpoint head-object --bucket $aws_bucket_name --key ${arrayData[0]}  2> /dev/null | jq .ETag | sed 's/\"//g' | sed 's/\\//g')
-	
+		s3ETag=$(aws s3api --endpoint https://$aws_endpoint head-object --bucket $aws_bucket_name --key ${arrayData[0]}  2> /dev/null | jq .ETag | sed 's/\"//g' | sed 's/\\//g')
+		
 
-	if [ -z "${s3ETag}" ]; then
-		echo "is not exist in the s3 storage: ${arrayData[0]}" >> ${LogFile}
-	else
+		if [ -z "${s3ETag}" ]; then
+			echo "is not exist in the s3 storage: ${arrayData[0]}" >> ${LogFile}
+		else
 
-		if [ "${s3ETag}" != "${arrayData[1]}" ]; then
-			echo "is not equal: ${arrayData[0]}" >> ${LogFile}
+			if [ "${s3ETag}" != "${arrayData[1]}" ]; then
+				echo "is not equal: ${arrayData[0]}" >> ${LogFile}
+			fi
 		fi
+
+		#tail -n +2 "/mnt/dataverse_checkETag.txt" > "/mnt/dataverse_checkETag.txt.tmp"
+		#mv "/mnt/dataverse_checkETag.txt.tmp" "/mnt/dataverse_checkETag.txt"
+
+		sed '1d' "/mnt/dataverse_checkETag.txt"
+
+		if [ ! -s "/mnt/dataverse_checkETag.txt" ]; then
+			exit 0
+		fi
+
+		sleep 1s
 	fi
-
-	#tail -n +2 "/mnt/dataverse_checkETag.txt" > "/mnt/dataverse_checkETag.txt.tmp"
-	#mv "/mnt/dataverse_checkETag.txt.tmp" "/mnt/dataverse_checkETag.txt"
-
-	sed '1d' "/mnt/dataverse_checkETag.txt"
-
-	if [ ! -s "/mnt/dataverse_checkETag.txt" ]; then
-		exit 0
-	fi
-
-	sleep 1s
 done
 
 
