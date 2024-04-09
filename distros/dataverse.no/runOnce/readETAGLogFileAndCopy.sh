9109bf0a3d87566222d342141d52806c8bd57e4d
root <loic.bergeron@uit.no>
2024-04-09 07:26:05 +0000

    remmoved old URL

--- distros/dataverse.no/runOnce/readETAGLogFileAndCopy.sh
+++ distros/dataverse.no/runOnce/readETAGLogFileAndCopy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AccessURL="https://pdvtstdatanfs.blob.core.windows.net/data1?sp=rcw&st=2023-03-27T11:20:31Z&se=2023-03-28T19:20:31Z&spr=https&sv=2021-12-02&sr=c&sig=2UTzVDaCoxBvfHO2pFKeU5saQipSJs9m4nEg9XgC0oY%3D"
+AccessURL="[AZURE_BLOB_URL]"
 BaseFolder="/dataverse/dataverse-files"
 LogFile="./checkETAG_2024.log"
 
