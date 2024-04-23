ad3d5eecd1d503f502e5b8f8c796c5657a35307a
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2024-04-23 08:24:35 +0000

    fixed checkETagByFiles.sh and curl call

--- distros/dataverse.no/init.d/201-bundle.sh
+++ distros/dataverse.no/init.d/201-bundle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 /usr/bin/apt-get install patch -y
-/usr/bin/curl -z -o $DOCROOT_DIR/Bundle.properties.patch  https://raw.githubusercontent.com/DataverseNO/dataverse-docker/dataverse.no/distros/dataverse.no/modification/Bundle.properties.patch
+curl -z -o $DOCROOT_DIR/Bundle.properties.patch  https://raw.githubusercontent.com/DataverseNO/dataverse-docker/dataverse.no/distros/dataverse.no/modification/Bundle.properties.patch
 /usr/bin/patch /opt/payara/appserver/glassfish/domains/domain1/applications/dataverse/WEB-INF/classes/propertyFiles/Bundle.properties $DOCROOT_DIR/Bundle.properties.patch
